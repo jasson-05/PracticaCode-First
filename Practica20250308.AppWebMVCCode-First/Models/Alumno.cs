1688a08146e07f7975d09ffdde5bd0a2046cf1ec
Jasso <Jasso@DESKTOP-97AP7FC>
2025-03-08 22:09:52 -0600

    Clase Alumno, Empleado paquetes NuGet descargados

---
+++ Practica20250308.AppWebMVCCode-First/Models/Alumno.cs
@@ -0,0 +1,14 @@
+﻿namespace Practica20250308.AppWebMVCCode_First.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Codigo { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public short YearEstudio { get; set; }
+
+    }
+}
---
+++ Practica20250308.AppWebMVCCode-First/Models/Empleado.cs
@@ -0,0 +1,14 @@
+﻿namespace Practica20250308.AppWebMVCCode_First.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Dui { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+    }
+
+}
+
